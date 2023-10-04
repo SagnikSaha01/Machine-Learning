bdbd82be5aaa726414698b4e6a4b1de24c39c15e
SagnikSaha01 <134161766+SagnikSaha01@users.noreply.github.com>
2023-10-04 00:52:42 -0400

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,10 +6,8 @@ import numpy as np
 
 
 model = YOLO("C:\\Users\\someone\\Desktop\\yolov8\\runs\\detect\\train22\\weights\\last.pt")
-#model = YOLO('./runs/classify/train14/weights/last.pt')  # load a custom model
 #liveResults = model.predict(source = "0",show = True)
 
-#results = model('./data/weather_dataset/train/sunrise/sunrise1.jpg')  # predict on an image
 camera = cv2.VideoCapture(0)
 
 box_annotations = sv.BoxAnnotator(thickness = 2,text_thickness = 2,text_scale = 1,)
